84d8fc7809d221055d86cdb378b087e8ee2bffa3
Vyacheslav Lobanov <bartoldo@bk.ru>
2022-06-23 17:03:55 +0300

    Refactored.

--- nosorog/decorators/protect_private.py
+++ nosorog/decorators/protect_private.py
@@ -3,7 +3,8 @@ import re
 
 from nosorog.decorators.nosorog_base_decorator import NosorogBaseDecorator
 from nosorog.exceptions.mixins.nosorog_exception_messages import NosorogExceptionMessages
-from nosorog.exceptions import NosorogMangledNameError, NosorogWrongPlaceCallError, NosorogWentWrongError
+from nosorog.exceptions import NosorogMangledNameError, NosorogWrongPlaceCallError, NosorogWentWrongError, \
+                                    NosorogFakeError
 from nosorog.decorators.metaclasses.protect_private_meta import ProtectPrivateMeta
 
 
@@ -12,24 +13,43 @@ class ProtectPrivate(NosorogBaseDecorator, metaclass=ProtectPrivateMeta):
     def __init__(self, *, func, attrs=None, protection_method=None):
         super().__init__(func=func)
         self.attrs = attrs
-
-        if protection_method:
-            methods = {
-                'block_if_not_self': self.__block_if_not_self,
-                'block_if_wrong_method': self.__block_if_wrong_method,
-                'block_if_not_in_list': self.__block_if_not_in_list,
-                'block_if_mangled': self.__block_if_mangled,
-            }
-            self.protection_method = methods.get(protection_method)
+        self.protection_method = protection_method
 
     def __call__(self, obj, *args, **kwargs):
 
         fn = inspect.stack()
+
         self.regexp = r"( self)*(\.)(_{class_name})*({function_name})\(".format(
             class_name=obj.__class__.__name__,
             function_name=self.func.__name__
         )
-        self.protection_method(fn)
+
+        protection_props = {
+            'block_if_not_self': (
+                                    lambda item, method_: NosorogFakeError if method_.group().startswith(' self')
+                                    else NosorogWrongPlaceCallError,
+                                    NosorogExceptionMessages.use_self,
+                                  ),
+            'block_if_wrong_method': (
+                                    lambda item, method_: NosorogFakeError if item.function == self.attrs
+                                    else NosorogWrongPlaceCallError, None,
+            ),
+            'block_if_not_in_list': (
+                                    lambda item, method_: NosorogFakeError if item.function in self.attrs
+                                    else NosorogWrongPlaceCallError, None,
+            ),
+            'block_if_mangled': (
+                                    lambda item, method_: NosorogFakeError if '.__' in method_.group()
+                                    else NosorogMangledNameError, None,
+            ),
+        }
+
+        func, message = protection_props.get(self.protection_method)
+
+        try:
+            raise self.__search_caller(fn, func)(message)
+        except NosorogFakeError:
+            pass
 
         try:
             result = super().__call__(obj, *args, **kwargs)
@@ -48,23 +68,3 @@ class ProtectPrivate(NosorogBaseDecorator, metaclass=ProtectPrivateMeta):
             caller_name = None
 
         return caller_name
-
-    def __block_if_not_self(self, fn):
-        func = lambda item, method_: False if method_.group().startswith(' self') else True
-        if self.__search_caller(fn, func):
-            raise NosorogWrongPlaceCallError(NosorogExceptionMessages.use_self)
-
-    def __block_if_not_in_list(self, fn):
-        func = lambda item, method_: item.function
-        if self.__search_caller(fn, func) not in self.attrs:
-            raise NosorogWrongPlaceCallError
-
-    def __block_if_wrong_method(self, fn):
-        func = lambda item, method_: item.function
-        if self.__search_caller(fn, func) != self.attrs:
-            raise NosorogWrongPlaceCallError
-
-    def __block_if_mangled(self, fn):
-        func = lambda item, method_: False if '.__' in method_.group() else True
-        if self.__search_caller(fn, func):
-            raise NosorogMangledNameError
