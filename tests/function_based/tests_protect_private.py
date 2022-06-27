ebcaff3c65b698b92701ebb3c6038d421abf07e3
Vyacheslav Lobanov <bartoldo@bk.ru>
2022-06-27 17:07:35 +0300

    Names refactored with PEP8.

--- tests/function_based/tests_protect_private.py
+++ tests/function_based/tests_protect_private.py
@@ -8,22 +8,22 @@ from nosorog.exceptions.mixins.nosorog_exception_messages import NosorogExceptio
 
 
 class TestProtectPrivateDecorator(unittest.TestCase, Messages):
-    b = Example2(Example1())
+    instance = Example2(Example1())
 
     def test_protect_private_decorator_blocks_outside_call(self):
         with self.assertRaises(NosorogWrongPlaceCallError, msg=self.msg_wrong_type) as context:
-            self.b.var_1._Example1__func_1()
+            self.instance.var_1._Example1__func_1()
         self.assertTrue(NosorogExceptionMessages.wrong_place in str(context.exception),
                         msg=self.msg_wrong_exc)
 
     def test_protect_private_decorator_blocks_not_allowed_list_func_name(self):
         with self.assertRaises(NosorogWrongPlaceCallError, msg=self.msg_wrong_type) as context:
-            self.b.var_1.alter_func_1()
+            self.instance.var_1.alter_func_1()
         self.assertTrue(NosorogExceptionMessages.wrong_place in str(context.exception),
                         msg=self.msg_wrong_exc)
 
     def test_protect_private_decorator_bypass_allowed_list_func_name(self):
-        self.assertTrue(self.b.var_1.public_func() == self.b.var_1.var, msg="The decorator does not bypass allowed methods.")
+        self.assertTrue(self.instance.var_1.public_func() == self.instance.var_1.var, msg="The decorator does not bypass allowed methods.")
 
     def test_protect_private_decorator_bypass_allowed_list_with_self(self):
-        self.assertTrue(self.b.var_1.alter_func_2() == self.b.var_1.var, msg="The decorator does not bypass methods with self.")
+        self.assertTrue(self.instance.var_1.alter_func_2() == self.instance.var_1.var, msg="The decorator does not bypass methods with self.")
